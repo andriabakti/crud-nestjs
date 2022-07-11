969843281b47f802b101e9b4d82820d48ca19f73
Andria Bakti <andr.bkti@gmail.com>
2022-07-11 09:37:11 +0700

    Use ENV & change TypeORM config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
-import { typeOrmConfig } from './configs/typeorm.config';
+// import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+// import * as dotenv from 'dotenv';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), BooksModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: +process.env.PG_PORT,
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    BooksModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '3939smile',
-  database: 'db_book',
+  host: process.env.PG_HOST,
+  port: +process.env.PG_PORT,
+  username: process.env.PG_USERNAME,
+  password: `${process.env.PG_PASSWORD}`,
+  database: process.env.PG_DATABASE,
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
+  autoLoadEntities: true,
   synchronize: true,
 };
